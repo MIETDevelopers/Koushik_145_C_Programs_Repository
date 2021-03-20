b4a7d677bdd988f3db11fb41cab0ed7d518a5701
koushik-mahajan <76400170+koushik-mahajan@users.noreply.github.com>
2021-03-20 23:48:51 +0530

    hello world

---
+++ 3_hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Pre-Process directive to include standard input output header file
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
