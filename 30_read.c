6c2e2526b3f5f4621677ce039fbca4cf5849c55c
koushik-mahajan <76400170+koushik-mahajan@users.noreply.github.com>
2021-03-21 14:26:46 +0530

    Program to read and write in a file

---
+++ 30_read.c
@@ -0,0 +1,25 @@
+// Program to read and write in a file
+// Author == koushik mahajan
+#include <stdio.h> //input output header file.
+#include <stdlib.h>
+
+int main() { //main function body starting
+    char sentence[1000];
+
+    // creating file pointer to work with files
+    FILE *fptr;
+
+    // opening file in writing mode
+    fptr = fopen("program.txt", "w");
+
+    // exiting program 
+    if (fptr == NULL) {
+        printf("Error!");
+        exit(1);
+    }
+    printf("Enter a sentence:\n");
+    fgets(sentence, sizeof(sentence), stdin);
+    fprintf(fptr, "%s", sentence);
+    fclose(fptr);
+    return 0;
+}
