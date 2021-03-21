a256628c92523f564998da42b5576fe75a184d9d
koushik-mahajan <76400170+koushik-mahajan@users.noreply.github.com>
2021-03-21 12:58:45 +0530

    To demonstrate the use of various string operations

---
+++ 21_string.c
@@ -0,0 +1,21 @@
+//To demonstrate the use of various string operations
+//Author == koushik mahajan
+//21-03-2021
+
+#include <stdio.h> //standard input output header file
+#include <string.h> //string input output header file
+int main()
+{
+    /* String Declaration*/
+    char nickname[20];
+
+    /* Console display using puts */
+    puts("Enter your Nick name:");
+
+    /*Input using gets*/
+    gets(nickname);
+
+    puts(nickname);
+
+    return 0;
+} 
