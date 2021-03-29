7b83f7c69f636b4298402e371df99fd332eb58ff
koushik-mahajan <76400170+koushik-mahajan@users.noreply.github.com>
2021-03-30 02:49:39 +0530

    1D and 2D arrays passed as arguments to a single function and the function implementation should simply print those arrays passed as arguments

---
+++ 1d.c
@@ -0,0 +1,18 @@
+//1D and 2D arrays passed as arguments to a single function
+//Author koushik mahajan
+#include <stdio.h>
+void disp( char ch)
+{
+   printf("%c ", ch);
+}
+int main()
+{
+   char arr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+   for (int x=0; x<10; x++)
+   {
+       /* I’m passing each element one by one using subscript*/
+       disp (arr[x]);
+   }
+
+   return 0;
+}
